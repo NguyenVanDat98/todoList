1ff08ca51a1951493045bd9cdb039a4a136ec06d
Dat <nguyenvom98@gmail.com>
2022-07-24 13:22:46 +0700

    new

--- src/App.js
+++ src/App.js
@@ -5,12 +5,6 @@ import add from "./componentCard/data"
 import Sidebar from "./componentCard/sidebar/Sidebar";
 
 
-// function eventButton(){
-//   add().filter((e)=>{
-//     e.title ==="New"?
-//   } )
-// }
-
 function App(props) {
  
   // let data = getData("Datalists")
--- src/componentCard/data.js
+++ src/componentCard/data.js
@@ -2,16 +2,16 @@
 function add() {
   return [
     
-    { title: "cac", name: "v", mess: "j" , stt: "New"},
     { title: "c", name: "v", mess: "j" , stt: "New"},
-    // { title: "c", name: "v", mess: "j" , stt: "New"},
-    // { title: "c", name: "v", mess: "j" , stt: "New"},
-    // { title: "c", name: "v", mess: "j" , stt: "New"},
-    // { title: "c", name: "v", mess: "j" , stt: "New"},
-    // { title: "c", name: "v", mess: "j" , stt: "New"},
-    // { title: "c", name: "v", mess: "j" , stt: "New"},
-    // { title: "c", name: "v", mess: "j" , stt: "New"},
-    // { title: "c", name: "v", mess: "j" , stt: "New"},
+    { title: "c", name: "v", mess: "j" , stt: "New"},
+    { title: "c", name: "v", mess: "j" , stt: "New"},
+    { title: "c", name: "v", mess: "j" , stt: "New"},
+    { title: "c", name: "v", mess: "j" , stt: "New"},
+    { title: "c", name: "v", mess: "j" , stt: "New"},
+    { title: "c", name: "v", mess: "j" , stt: "New"},
+    { title: "c", name: "v", mess: "j" , stt: "New"},
+    { title: "c", name: "v", mess: "j" , stt: "New"},
+    { title: "c", name: "v", mess: "j" , stt: "New"},
   
     { title: "c", name: "v", mess: "adsasddasdasdasdasdasdasdasda asd asd as asd as as dasd asd asa  asd asd as asd asd as" },
    
