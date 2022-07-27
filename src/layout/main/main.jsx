8c652877175c1541be917abd68871d73453d20d4
Dat <nguyenvom98@gmail.com>
2022-07-27 20:48:35 +0700

    mai lam tiep

--- src/layout/main/main.jsx
+++ src/layout/main/main.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import add from "../../data";
 import Sidebar from "../../component/sidebar";
@@ -38,8 +38,8 @@ const Main = (props) => {
   const [index, setInd] = useState(0);
   const [change, setchange] = useState(true);
 //   const [Arrayy, setarray] = useState(arr[index]);
-  const [statePagination ,setState] = useState({next:true, pre:true})
-
+//   const [statePagination ,setState] = useState({next:true, pre:true})
+    // useEffect(runtime(index),[])
   function settxt(e) {
     console.log(e.target.id);
     arr[index].map((a, i) => {
@@ -59,35 +59,35 @@ const Main = (props) => {
     });
     setchange(!change);
   }
-  function runtime(e){
-    if (e<1){
-        document.getElementById("next").style.display="block"
-        document.getElementById("pre").style.display="none"
-
+  function runtime(e,a){
+    setchange(!change);
+    console.log(index);
+    if (e<a){
+       
        document.querySelectorAll(".check button").forEach((el,indexx)=>{
-            indexx<=2? el.style.display= "block":el.style.display= "none"
+            indexx<=2? el.style.display= "block": el.style.display= "none"
        })
     }else if (e == arr.length-1 ){
-        document.getElementById("next").style.display="none"
-        document.getElementById("pre").style.display="block"
+   
         document.querySelectorAll(".check button").forEach((el,indexx)=>{
             indexx<=arr.length-4? el.style.display= "none": el.style.display= "block"
         })
     }else {
-        document.getElementById("next").style.display="block"
-        document.getElementById("pre").style.display="block"
+       
 
         document.querySelectorAll(".check button").forEach((el,indexx)=>{
             (indexx >= e-1 && indexx-1 <= e)? el.style.display= "block" : el.style.display= "none"
         })
     }
+   
+
   }
   function clickk(e){
 
     aii.forEach((item , i) => {
       e.target.id == i ? setInd(i) : console.log();;
     });
-    runtime(e.target.id);
+    runtime(e.target.id ,1);
 
     document.querySelectorAll(".check button").forEach((el) => {
       el.classList.remove("active");
@@ -98,7 +98,7 @@ const Main = (props) => {
   }
   function nexts(){
     let temp=0;
-    runtime(index);
+    runtime(index, 1);
       if(index!==arr.length-1){
         document.querySelectorAll(".check button").forEach((el) => {
             if(el.classList.contains("active")){
@@ -107,15 +107,15 @@ const Main = (props) => {
             }else if(temp==1){
                 el.classList.add("active");
                 temp--;
-            }
+            } 
           });
           setInd(index+1)
       };
   }
   function prer() {
-    let temp =0;
+    let temp = 0;
     let cont;
-    runtime(index);
+    runtime(index , 1);
       if(index!==0){
         document.querySelectorAll(".check button").forEach((el,ii) => {
                 if(el.classList.contains("active")){
@@ -132,6 +132,9 @@ const Main = (props) => {
             });
         setInd(index-1);
       }
+       
+
+      
       
   }
 
@@ -159,17 +162,17 @@ const Main = (props) => {
         <div id="list" className="w-100">
           {<Todolist eventt={settxt} data={arr[index]} />}
         </div>
-        <div className="pagination mt-4 d-flex justify-content-center">
-            <button onClick={prer} id="pre">Pre</button>
-            <div className="check  mx-5">
+        <div className="pagination mt-4 d-flex justify-content-center" >
+            <button onClick={prer} id="pre" disabled={index==0 ? true : false}>Pre</button>
+            <div className="check mx-auto">
                 {
-                aii.map((arc, i) => (
-                    <button onClick={clickk} key={i} id={i}>
-                    {arc}
-                    </button>
-                ))}
+                   
+                aii.map((arc, i) => (                    
+                      <button onClick={clickk} key={i} id={i}> {arc}  </button>                     
+                ))
+                }
              </div>
-            <button onClick={nexts} id="next">Next</button>
+            <button onClick={nexts} id="next" disabled={index==arr.length-1 ? true : false}>Next</button>
         </div>
         
       </div>
